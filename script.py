946d69a0a52b5cbafc601aaf9d50827715a0c05d
Alexander Dean <alexjacobdean1@gmail.com>
2022-04-13 10:57:49 -0500

    More descriptive script

--- script.py
+++ script.py
@@ -6,6 +6,8 @@ subprocess.call(['sh', './shells/get_data.sh'])
 directory = os.fsencode("data/")
 files = os.listdir("data/")
 
+print("Partitioning images by age of individual...")
+
 for file_name in files:
     age = file_name.partition('_')[0]
     new_path = "data/" + age
@@ -14,3 +16,5 @@ for file_name in files:
         os.makedirs(new_path)
         
     shutil.move("data/" + file_name, new_path)
+
+print("Done! :)")
