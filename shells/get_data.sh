9b6967c9fd74e3ba8dd0825385636ab2d50a54a8
Alexander Dean <alexjacobdean1@gmail.com>
2022-04-13 11:54:09 -0500

    Split data set into training and test set, automate script

--- shells/get_data.sh
+++ shells/get_data.sh
@@ -1,6 +1,11 @@
+echo "Starting now. Downloading the data set..."
 kaggle datasets download -d jangedoo/utkface-new
 unzip utkface-new.zip
+
+echo "Removing unnecessary folders and files..."
 rm -r utkface-new.zip
 rm -r utkface_aligned_cropped
 rm -r crop_part1
+
+echo "Renaming the data file..."
 mv UTKFace/ data/
