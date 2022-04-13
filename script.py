f301045fdb8902b47f3eff616492a02b41702e59
Alexander Dean <alexjacobdean1@gmail.com>
2022-04-13 10:52:15 -0500

    Add shell and script to pull data, organize data

---
+++ script.py
@@ -0,0 +1,16 @@
+import os
+import shutil
+import subprocess
+
+subprocess.call(['sh', './shells/get_data.sh'])
+directory = os.fsencode("data/")
+files = os.listdir("data/")
+
+for file_name in files:
+    age = file_name.partition('_')[0]
+    new_path = "data/" + age
+    
+    if not os.path.exists(new_path):
+        os.makedirs(new_path)
+        
+    shutil.move("data/" + file_name, new_path)
---
+++ shells/get_data.sh
@@ -0,0 +1,6 @@
+kaggle datasets download -d jangedoo/utkface-new
+unzip utkface-new.zip
+rm -r utkface-new.zip
+rm -r utkface_aligned_cropped
+rm -r crop_part1
+mv UTKFace/ data/
